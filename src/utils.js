1f78280b8819b23096c9355655ca68efffe84338
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 18:14:19 +0300

    Change export syntax

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 'use strict';
 
-exports.getRandomInt = (min, max) => {
+module.exports.getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-exports.shuffle = (someArray) => {
+module.exports.shuffle = (someArray) => {
   for (let i = someArray.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
     [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
@@ -14,6 +14,6 @@ exports.shuffle = (someArray) => {
   return someArray;
 };
 
-exports.getPictureFileName = (number) => {
+module.exports.getPictureFileName = (number) => {
   return `item${number.toString().padStart(2, 0)}.jpg`;
 };
