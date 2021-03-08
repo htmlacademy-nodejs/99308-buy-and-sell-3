855a495874db2f62efa9c3f95061a38b3c7fde5e
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 16:54:22 +0300

    Add help command

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 
 const Cli = {
+  [help.name]: help,
   [generate.name]: generate,
   [version.name]: version
 };
