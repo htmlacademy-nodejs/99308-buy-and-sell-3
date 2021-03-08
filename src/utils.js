41ab91dec6dd812e2db8a3e1f37cda829dcc2960
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 22:16:54 +0300

    Unite export commands

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports.getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-module.exports.shuffle = (someArray) => {
+const shuffle = (someArray) => {
   for (let i = someArray.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
     [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
@@ -14,6 +14,8 @@ module.exports.shuffle = (someArray) => {
   return someArray;
 };
 
-module.exports.getPictureFileName = (number) => {
+const getPictureFileName = (number) => {
   return `item${number.toString().padStart(2, 0)}.jpg`;
 };
+
+module.exports = {getRandomInt, shuffle, getPictureFileName};
