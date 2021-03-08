56203dc1fc62d3bd7dd3aeb2ce8752c50b796ee3
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 15:29:03 +0300

    Add cli index file

---
+++ src/service/cli/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const generate = require(`./generate`);
+const version = require(`./version`);
+
+const Cli = {
+  [generate.name]: generate,
+  [version.name]: version
+};
+
+module.exports = {
+  Cli
+};
