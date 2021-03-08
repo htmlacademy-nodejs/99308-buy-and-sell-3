7d00735ff6b6cc7286c4ba7b6f364c387a9e20f0
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 16:33:21 +0300

    Make app endpoint

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+const {USER_ARGV_INDEX, ExitCode} = require(`../constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+
+if (userArguments.length === 0 || !Cli[userArguments.slice(0, 1)]) {
+  process.exit(ExitCode.SUCCESS);
+}
+
+Cli[userArguments.slice(0, 1)].run(userArguments.slice(1));
