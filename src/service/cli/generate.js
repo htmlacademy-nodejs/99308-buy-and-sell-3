4564ba0a5957725a28608c124018ac57fdbbb09b
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 18:16:27 +0300

    Edit formatting

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,16 +1,36 @@
 'use strict';
 
-const {FILE_NAME, DEFAULT_COUNT, MAX_OFFERS, CATEGORIES, SENTENCES, TITLES, OfferType, SumRestrict, PictureRestrict, ExitCode} = require(`../../constants`);
+const {
+  FILE_NAME,
+  DEFAULT_COUNT,
+  MAX_OFFERS,
+  CATEGORIES,
+  SENTENCES,
+  TITLES,
+  OfferType,
+  SumRestrict,
+  PictureRestrict,
+  ExitCode
+} = require(`../../constants`);
+
 const {getRandomInt, shuffle, getPictureFileName} = require(`../../utils`);
+
 const fs = require(`fs`);
 
 const generateOffers = (count) => (
   Array(count).fill({}).map(() => ({
     category: shuffle(CATEGORIES).slice(0, getRandomInt(1, 2)),
     description: shuffle(SENTENCES).slice(1, 5).join(` `),
-    picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
+    picture: getPictureFileName(getRandomInt(
+        PictureRestrict.MIN,
+        PictureRestrict.MAX
+    )),
     title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
+    type: OfferType[
+      Object.keys(OfferType)[
+        Math.floor(Math.random() * Object.keys(OfferType).length)
+      ]
+    ],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX)
   }))
 );
