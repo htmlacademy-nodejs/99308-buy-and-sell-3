ea7d901d930392b3abf6370a5ea7d07e494d6aee
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-03-08 17:23:57 +0300

    Add default cli command

--- src/service/service.js
+++ src/service/service.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const {Cli} = require(`./cli`);
-const {USER_ARGV_INDEX, ExitCode} = require(`../constants`);
+const {DEFAULT_COMMAND, USER_ARGV_INDEX, ExitCode} = require(`../constants`);
 
 const userArguments = process.argv.slice(USER_ARGV_INDEX);
 
 if (userArguments.length === 0 || !Cli[userArguments.slice(0, 1)]) {
+  Cli[DEFAULT_COMMAND].run();
   process.exit(ExitCode.SUCCESS);
 }
 
