31b0f698d6e5f3934e21266cf03db101b024d86e
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-03 11:55:13 +0300

    Move mock elements to file

--- src/constants.js
+++ src/constants.js
@@ -6,35 +6,9 @@ const MAX_OFFERS = 1000;
 const FILE_NAME = `mocks.json`;
 const USER_ARGV_INDEX = 2;
 
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
+const CATEGORIES_FILE_PATH = `../../data/categories.txt`;
+const SENTENCES_FILE_PATH = `../../data/sentences.txt`;
+const TITLES_FILE_PATH = `../../data/titles.txt`;
 
 const OfferType = {
   OFFER: `offer`,
@@ -56,4 +30,4 @@ const ExitCode = {
   ERROR: 1
 };
 
-module.exports = {DEFAULT_COUNT, DEFAULT_COMMAND, MAX_OFFERS, FILE_NAME, USER_ARGV_INDEX, CATEGORIES, SENTENCES, TITLES, OfferType, SumRestrict, PictureRestrict, ExitCode};
+module.exports = {DEFAULT_COUNT, DEFAULT_COMMAND, MAX_OFFERS, FILE_NAME, USER_ARGV_INDEX, CATEGORIES_FILE_PATH, SENTENCES_FILE_PATH, TITLES_FILE_PATH, OfferType, SumRestrict, PictureRestrict, ExitCode};
