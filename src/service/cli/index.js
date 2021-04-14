fd0d02dc0d43fc4a3784d4b844f6b656724ccaef
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-14 04:30:52 +0300

    Add server cli command

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -3,11 +3,13 @@
 const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
+const server = require(`./server`);
 
 const Cli = {
   [help.name]: help,
   [generate.name]: generate,
-  [version.name]: version
+  [version.name]: version,
+  [server.name]: server
 };
 
 module.exports = {
