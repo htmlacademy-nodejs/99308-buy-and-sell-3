246b8a5f72f5905017fe31c8a3d19dd930152800
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-17 20:56:03 +0300

    Add offers routes

--- src/express/index.js
+++ src/express/index.js
@@ -3,10 +3,12 @@
 const express = require(`express`);
 const mainRouter = require(`./routes/main-routes`);
 const myRouter = require(`./routes/my-routes`);
+const offersRouter = require(`./routes/offers-routes`);
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
+app.use(`/offers`, offersRouter);
 app.listen(DEFAULT_PORT);
