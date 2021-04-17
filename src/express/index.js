e1c140edd34a67e93d82141dcde955a23b848c6d
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-04-17 20:21:25 +0300

    Add main routes

--- src/express/index.js
+++ src/express/index.js
@@ -1,18 +1,10 @@
 'use strict';
 
 const express = require(`express`);
+const mainRouter = require(`./routes/main-routes`);
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.listen(DEFAULT_PORT)
-  .on(`listening`, () => {
-    console.log(`Express listening at http://localhost:8080`);
-  })
-  .on(`error`, (err) => {
-    console.log(`Express error: `, err);
-  });
-
-app.get(`/`, (req, res) => {
-  res.send(`/`);
-});
+app.use(mainRouter);
+app.listen(DEFAULT_PORT);
