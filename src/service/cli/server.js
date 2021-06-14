772599c5fc2c510439bd037cbf833bf73961f139
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-06-14 22:18:36 +0300

    Remake app and router as local vars

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -8,27 +8,34 @@ const {HttpCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 const FILE = `./mocks.json`;
-const app = express();
-const offersRouter = new Router();
-
-app.use(express.json());
-
-offersRouter.get(`/`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILE);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (err) {
-    console.error(chalk.red(err));
-    res.json([]);
-  }
-});
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+
+    const app = express();
+    const offersRouter = new Router();
+
+    app.use(express.json());
+
+    offersRouter.get(`/`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILE);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (err) {
+        console.error(chalk.red(err));
+        res.json([]);
+      }
+    });
+
+    app.use(`/offers`, offersRouter);
+    app.use((req, res) => {
+      res.status(HttpCode.NOT_FOUND).send(`Not found`);
+    });
+
     app
       .listen(port)
       .on(`listening`, () => {
@@ -39,9 +46,5 @@ module.exports = {
       .on(`error`, (err) => {
         return console.error(chalk.red(`Create server error`, err));
       });
-    app.use(`/offers`, offersRouter);
-    app.use((req, res) => {
-      res.status(HttpCode.NOT_FOUND).send(`Not found`);
-    });
   }
 };
