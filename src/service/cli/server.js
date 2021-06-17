bf4c07c851af315f72c799aa464ed0376f7413b1
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-06-17 05:52:33 +0300

    Update server module

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,40 +1,27 @@
 'use strict';
 
 const express = require(`express`);
-const {Router} = require(`express`);
-const fs = require(`fs`).promises;
+const routes = require(`../api`);
 const chalk = require(`chalk`);
-const {HttpCode} = require(`../../constants`);
+const {HttpCode, API_PREFIX} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
-const FILE = `./mocks.json`;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(API_PREFIX, routes);
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).send(`Not found`);
+});
 
 module.exports = {
   name: `--server`,
   async run(args) {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-
-    const app = express();
-    const offersRouter = new Router();
-
-    app.use(express.json());
-
-    offersRouter.get(`/`, async (req, res) => {
-      try {
-        const fileContent = await fs.readFile(FILE);
-        const mocks = JSON.parse(fileContent);
-        res.json(mocks);
-      } catch (err) {
-        console.error(chalk.red(err));
-        res.json([]);
-      }
-    });
-
-    app.use(`/offers`, offersRouter);
-    app.use((req, res) => {
-      res.status(HttpCode.NOT_FOUND).send(`Not found`);
-    });
 
     app
       .listen(port)
