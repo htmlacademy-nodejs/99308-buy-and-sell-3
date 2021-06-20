7d20a1f40d5b4383bd3f563fa56536cfed0c9db9
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-06-20 23:28:54 +0300

    Make new routes as local variable

--- src/service/api/category.js
+++ src/service/api/category.js
@@ -3,9 +3,8 @@
 const {Router} = require(`express`);
 const {HttpCode} = require(`../../constants`);
 
-const route = new Router();
-
 module.exports = (app, service) => {
+  const route = new Router();
   app.use(`/category`, route);
   route.get(`/`, (req, res) => {
     const categories = service.findAll();
