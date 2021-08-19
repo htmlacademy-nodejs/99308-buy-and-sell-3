124afe9daf67fdc2ede72385bdd66c68753c768a
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-08-19 23:07:21 +0300

    Fix isDevMode variable

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -4,7 +4,7 @@ const pino = require(`pino`);
 const {Env} = require(`../../constants`);
 
 const LOG_FILE = `./logs/api.log`;
-const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
+const isDevMode = process.env.NODE_ENV !== Env.PRODUCTION;
 const defaultLogLevel = `info`;
 
 const logger = pino({
