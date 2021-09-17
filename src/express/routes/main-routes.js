8a14d08d72351971c2fb2295400465cf4889c089
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-09-17 21:14:03 +0300

    Use new express api in main router

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,8 +3,12 @@
 const {Router} = require(`express`);
 
 const mainRouter = new Router();
+const api = require(`../api`).getAPI();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main`, {offers});
+});
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search-result`));
