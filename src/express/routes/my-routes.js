15a1f3eec7b3afe803807a88ddfa6ba41ebcbb45
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-10-30 20:41:17 +0300

    Use new api in my-routes

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,8 +3,15 @@
 const {Router} = require(`express`);
 
 const myRouter = new Router();
+const api = require(`../api`).getAPI();
 
-myRouter.get(`/`, (req, res) => res.render(`my-tickets`));
-myRouter.get(`/comments`, (req, res) => res.render(`comments`));
+myRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`my-tickets`, {offers});
+});
+myRouter.get(`/comments`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`comments`, {offers});
+});
 
 module.exports = myRouter;
