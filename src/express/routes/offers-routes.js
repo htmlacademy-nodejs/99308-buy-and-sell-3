b493dec657dbc017b46d73b790c966a9b932faf7
Nikolay Zvyagin <nik.zvyagin@gmail.com>
2021-11-04 23:11:34 +0300

    Change offer edit modules

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -3,11 +3,19 @@
 const {Router} = require(`express`);
 
 const offersRouter = new Router();
+const api = require(`../api`).getAPI();
 
 offersRouter.get(`/`, (req, res) => res.render(`category`));
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`ticket-edit`, {offer, categories});
+});
 offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
 
 module.exports = offersRouter;
